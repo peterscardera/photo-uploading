8df12f3c99b3995b1932de7cc1df85dab2eb603f
PeterScardera <peterscardera@gmail.com>
2020-07-19 14:35:35 -0400

    feat: resolved querys with mdb collections

---
+++ backend/resolvers/Query.js
@@ -0,0 +1,11 @@
+module.exports = {
+  totalPhotos: (parent, args, { db }) =>
+    db.collection("photos").estimatedDocumentCount(),
+
+  totalUsers: (parent, args, { db }) =>
+    db.collection("users").estimatedDocumentCount(),
+
+  allPhotos: (parent, args, { db }) => db.collection("photos").find().toArray(),
+
+  allUsers: (parent, args, { db }) => db.collection("users").find().toArray(),
+};
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
-const expressPlayground = require("graphql-playground-middleware-express")
-  .default;
+const expressPlayground = require("graphql-playground-middleware-express").default;
 const { readFileSync } = require("fs");
 
 const resolvers = require("./resolvers");
@@ -12,10 +11,12 @@ const typeDefs = readFileSync("./backend/typeDefs.graphql", "UTF-8");
 
 const app = express();
 
+//connect to the database first
 initDb().then((resp) => {
   if (resp) {
-    //adding the connection to a context object
-    const context = { resp };
+    //adding the database to a context object and giving it name db
+    const db = resp.db("photoUploadingGQL");
+    const context = { db };
     const server = new ApolloServer({ typeDefs, resolvers, context });
 
     //call `applyMiddleware()` to allow middleware mounted on the same path
